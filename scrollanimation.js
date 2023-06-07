09364abce6e04c10131cf0964c62e3ae19cad956
Auggidog <81528976+Auggidog@users.noreply.github.com>
2023-06-07 20:59:41 +0200

    Add files via upload

---
+++ scrollanimation.js
@@ -0,0 +1,26 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+    
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 50, function(){
+
+       
+        
+      });
+    } // End if
+  });
+});
